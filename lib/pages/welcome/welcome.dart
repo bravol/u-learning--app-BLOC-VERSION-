cd862bf00d4428826123ebf79b1eb0b750e8ed89
Brian Bravol <lumalabravo@gmail.com>
2024-07-20 21:19:14 +0300

    we are usig bloc in every thing

---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,150 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:u_learning_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:u_learning_app/pages/welcome/bloc/welcome_states.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        body: BlocBuilder<WelcomeBloc, WelcomeState>(
+          builder: (context, state) {
+            return Container(
+              margin: EdgeInsets.only(top: 34.h),
+              width: 375.w,
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  PageView(
+                    onPageChanged: (index) {
+                      print(index);
+                    },
+                    children: [
+                      _page(
+                        index: 0,
+                        context: context,
+                        buttomName: 'Next',
+                        name: 'First see Learning',
+                        subtitle:
+                            'Forget about a for of paper all knowledge in on learning',
+                        imagePath: 'Image path',
+                      ),
+                      _page(
+                        index: 1,
+                        context: context,
+                        buttomName: 'Next',
+                        name: 'Connect with everyone',
+                        subtitle:
+                            'Always keep in touch with your tuitor and lets get connected',
+                        imagePath: 'Image path',
+                      ),
+                      _page(
+                        index: 2,
+                        context: context,
+                        buttomName: 'Get Started',
+                        name: 'Always Fascinated Learning',
+                        subtitle:
+                            'Anyone, anytime, the time is at our description on study whenever you want',
+                        imagePath: 'Image path',
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: 100.h,
+                    child: DotsIndicator(
+                      dotsCount: 3,
+                      decorator: DotsDecorator(
+                          color: Colors.grey,
+                          size: const Size.square(8.0),
+                          activeColor: Colors.blue,
+                          activeShape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _page(
+      {required index,
+      required BuildContext context,
+      required String buttomName,
+      required String name,
+      required String subtitle,
+      required String imagePath}) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 345.w,
+          height: 345.w,
+          child: Text(imagePath),
+        ),
+        Container(
+          child: Text(
+            name,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(
+            subtitle,
+            style: TextStyle(
+              fontSize: 14.sp,
+              color: Colors.black.withOpacity(0.5),
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+          width: 325.w,
+          height: 50.h,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(
+              Radius.circular(15.w),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              )
+            ],
+          ),
+          child: Center(
+            child: Text(
+              buttomName,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.normal),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
