f66a584ed0d07a362033cc816a008409d0153b99
Brian Bravol <lumalabravo@gmail.com>
2024-07-20 22:31:05 +0300

    finished welcoming pages/landing pages

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -2,7 +2,9 @@ import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:u_learning_app/pages/home/home.dart';
 import 'package:u_learning_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:u_learning_app/pages/welcome/bloc/welcome_events.dart';
 import 'package:u_learning_app/pages/welcome/bloc/welcome_states.dart';
 
 class Welcome extends StatefulWidget {
@@ -13,6 +15,7 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,46 +30,50 @@ class _WelcomeState extends State<Welcome> {
                 alignment: Alignment.topCenter,
                 children: [
                   PageView(
+                    controller: pageController,
                     onPageChanged: (index) {
-                      print(index);
+                      state.page = index;
+                      BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
                     },
                     children: [
                       _page(
-                        index: 0,
+                        index: 1,
                         context: context,
                         buttomName: 'Next',
                         name: 'First see Learning',
                         subtitle:
                             'Forget about a for of paper all knowledge in on learning',
-                        imagePath: 'Image path',
+                        imagePath: 'assets/images/reading.png',
                       ),
                       _page(
-                        index: 1,
+                        index: 2,
                         context: context,
                         buttomName: 'Next',
                         name: 'Connect with everyone',
                         subtitle:
                             'Always keep in touch with your tuitor and lets get connected',
-                        imagePath: 'Image path',
+                        imagePath: 'assets/images/boy.png',
                       ),
                       _page(
-                        index: 2,
+                        index: 3,
                         context: context,
                         buttomName: 'Get Started',
                         name: 'Always Fascinated Learning',
                         subtitle:
                             'Anyone, anytime, the time is at our description on study whenever you want',
-                        imagePath: 'Image path',
+                        imagePath: 'assets/images/man.png',
                       ),
                     ],
                   ),
                   Positioned(
                     bottom: 100.h,
                     child: DotsIndicator(
+                      position: state.page,
                       dotsCount: 3,
                       decorator: DotsDecorator(
                           color: Colors.grey,
                           size: const Size.square(8.0),
+                          activeSize: const Size(18, 8),
                           activeColor: Colors.blue,
                           activeShape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(5.0))),
@@ -93,7 +100,10 @@ class _WelcomeState extends State<Welcome> {
         SizedBox(
           width: 345.w,
           height: 345.w,
-          child: Text(imagePath),
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.cover,
+          ),
         ),
         Container(
           child: Text(
@@ -116,31 +126,46 @@ class _WelcomeState extends State<Welcome> {
             ),
           ),
         ),
-        Container(
-          margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
-          width: 325.w,
-          height: 50.h,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.all(
-              Radius.circular(15.w),
+        GestureDetector(
+          onTap: () {
+            //0-2 index
+            if (index < 3) {
+              //aniation
+              pageController.animateToPage(index,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.decelerate);
+            } else {
+              //jump to new page
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => const Home()));
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+            width: 325.w,
+            height: 50.h,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.w),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1),
+                )
+              ],
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 1,
-                blurRadius: 2,
-                offset: const Offset(0, 1),
-              )
-            ],
-          ),
-          child: Center(
-            child: Text(
-              buttomName,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.normal),
+            child: Center(
+              child: Text(
+                buttomName,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.normal),
+              ),
             ),
           ),
         )
